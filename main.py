79bc59615025b55c95942ad713fb9b2d139b518b
Neo <mrrneo@outlook.com>
2025-09-22 03:17:07 +0600

    ...

---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi import HTTPException
+from PIL import Image
+from fastapi import FastAPI, UploadFile, File
+from torchvision import transforms
+import torch.nn as nn
+import uvicorn
+import torch
+import io
+
+
+app = FastAPI()
+
+class CheckImage(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.first = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.second = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(16 * 14 * 14, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+    def forward(self, x):
+        x = self.first(x)
+        x = self.second(x)
+        return x
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor()
+])
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = CheckImage()
+model.load_state_dict(torch.load('model.pth', map_location=device))
+model.to(device)
+model.eval()
+
+@app.post('/predict')
+async def check_image(file: UploadFile = File()):
+    try:
+        image_bytes = await file.read()
+        if not image_bytes:
+            raise HTTPException(400, detail='Файл кошулган жок')
+
+        image = Image.open(io.BytesIO(image_bytes))
+        image_tensor = transform(image).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            y_prediction = model(image_tensor)
+            prediction = y_prediction.argmax(dim=1).item()
+        return {'Answer': prediction}
+
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
